ec99859b3c51573d7c85f016114a04609ea6c733
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2023-02-11 10:40:14 +0000

    Initial commit for camera

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use bevy::{prelude::*, render::render_resource::*};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+mod camera;
+use camera::*;
+
+pub const SCREEN_DIMENSIONS: (f32, f32) = (1024.0, 768.0);
+
+fn main() {
+    App::new()    
+        .insert_resource(ClearColor(Color::rgb_u8(0, 0, 0)))
+        .add_plugins(
+            DefaultPlugins
+                .set(AssetPlugin {
+                    watch_for_changes: true,
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        width: SCREEN_DIMENSIONS.0,
+                        height: SCREEN_DIMENSIONS.1,
+                        title: "Tenebris".into(),
+                        resizable: false,
+                        mode: WindowMode::Windowed,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .set(ImagePlugin {
+                    default_sampler: SamplerDescriptor {
+                        mag_filter: FilterMode::Nearest,
+                        min_filter: FilterMode::Nearest,
+                        ..default()
+                    },
+                }),
+        )
+        .add_plugin(WorldInspectorPlugin)
+        .add_startup_system(setup)
+
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("test.png"),
+        ..default()
+    });
+}
