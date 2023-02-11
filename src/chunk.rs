14e7a91dc9c90b23492e12a8483d4a8d28a3ac85
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2023-02-11 13:29:33 +0000

    Add some more to chunk.rs

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,17 +1,39 @@
-use bevy::{prelude::*, utils::HashSet};
+use bevy::{math::*, prelude::*, utils::HashSet};
 use bevy_ecs_tilemap::prelude::*;
+
+pub const RENDER_CHUNK_SIZE: UVec2 = UVec2 { x: 8, y: 8 };
+
+pub const TILE_SIZE: UVec2 = UVec2 { x: 8, y: 8 };
 
 #[derive(Debug, Default, Resource)]
 pub struct ChunkManager {
-    pub spawned_chunks: HashSet<IVec2>,
+	pub spawned_chunks: HashSet<IVec2>,
 }
 
-pub fn spawn_chunks(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    camera_query: Query<&Transform, With<Camera>>,
-    mut chunk_manager: ResMut<ChunkManager>,
-) {
+fn chunk_position(position: &Vec2) -> IVec2 {
+	position.as_ivec2() / (RENDER_CHUNK_SIZE * TILE_SIZE).as_ivec2()
+}
+
+pub fn spawn_chunk(commands: &mut Commands, asset_server: &AssetServer, chunk_position: IVec2) {
+	let tilemap_entity = commands.spawn_empty().id();
+	let mut tile_storage = TileStorage::empty(RENDER_CHUNK_SIZE.into());
+	for x in 0..RENDER_CHUNK_SIZE.x {
+		for y in 0..RENDER_CHUNK_SIZE.y {}
+	}
+}
+
+pub fn spawn_chunks(mut commands: Commands, asset_server: Res<AssetServer>, cameras: Query<&Transform, With<Camera>>, mut chunk_manager: ResMut<ChunkManager>) {
+	for camera in cameras.iter() {
+		let camera_chunk_position = chunk_position(&camera.translation.xy());
+		for x in camera_chunk_position.x - 2..camera_chunk_position.x + 2 {
+			for y in camera_chunk_position.y - 2..camera_chunk_position.y + 2 {
+				let chunk_position = IVec2::new(x, y);
+				if chunk_manager.spawned_chunks.contains(&chunk_position) {
+					chunk_manager.spawned_chunks.insert(chunk_position);
+				}
+			}
+		}
+	}
 }
 
 pub fn despawn_chunks() {}
--- src/main.rs
+++ src/main.rs
@@ -12,50 +12,45 @@ pub const SCREEN_DIMENSIONS: (f32, f32) = (1024.0, 768.0);
 pub const RENDER_CHUNK_SIZE: UVec2 = UVec2 { x: 8, y: 8 };
 
 fn main() {
-    App::new()
-        .insert_resource(ClearColor(Color::rgb_u8(0, 0, 0)))
-        .add_plugins(
-            DefaultPlugins
-                .set(AssetPlugin {
-                    watch_for_changes: true,
-                    ..default()
-                })
-                .set(WindowPlugin {
-                    window: WindowDescriptor {
-                        width: SCREEN_DIMENSIONS.0,
-                        height: SCREEN_DIMENSIONS.1,
-                        title: "Tenebris".into(),
-                        resizable: false,
-                        mode: WindowMode::Windowed,
-                        ..default()
-                    },
-                    ..default()
-                })
-                .set(ImagePlugin {
-                    default_sampler: SamplerDescriptor {
-                        mag_filter: FilterMode::Nearest,
-                        min_filter: FilterMode::Nearest,
-                        ..default()
-                    },
-                }),
-        )
-        .insert_resource(TilemapRenderSettings {
-            render_chunk_size: RENDER_CHUNK_SIZE,
-        })
-        .add_plugin(TilemapPlugin)
-        .insert_resource(ChunkManager::default())
-        .add_plugin(WorldInspectorPlugin)
-        .add_startup_system(setup)
-        .add_system(update_camera)
-        .add_system(spawn_chunks)
-        .add_system(despawn_chunks)
-        .run();
+	App::new()
+		.insert_resource(ClearColor(Color::rgb_u8(0, 0, 0)))
+		.add_plugins(
+			DefaultPlugins
+				.set(AssetPlugin { watch_for_changes: true, ..default() })
+				.set(WindowPlugin {
+					window: WindowDescriptor {
+						width: SCREEN_DIMENSIONS.0,
+						height: SCREEN_DIMENSIONS.1,
+						title: "Tenebris".into(),
+						resizable: false,
+						mode: WindowMode::Windowed,
+						..default()
+					},
+					..default()
+				})
+				.set(ImagePlugin {
+					default_sampler: SamplerDescriptor {
+						mag_filter: FilterMode::Nearest,
+						min_filter: FilterMode::Nearest,
+						..default()
+					},
+				}),
+		)
+		.insert_resource(TilemapRenderSettings { render_chunk_size: RENDER_CHUNK_SIZE })
+		.add_plugin(TilemapPlugin)
+		.insert_resource(ChunkManager::default())
+		.add_plugin(WorldInspectorPlugin)
+		.add_startup_system(setup)
+		.add_system(update_camera)
+		.add_system(spawn_chunks)
+		.add_system(despawn_chunks)
+		.run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("test.png"),
-        ..default()
-    });
+	commands.spawn(Camera2dBundle::default());
+	commands.spawn(SpriteBundle {
+		texture: asset_server.load("test.png"),
+		..default()
+	});
 }
