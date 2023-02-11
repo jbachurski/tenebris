2d7b7c3039e6e1ea844c4ac03d251d09d874e925
jbachurski <kbachurski@gmail.com>
2023-02-11 17:40:15 +0000

    Make heading better

--- src/enemy.rs
+++ src/enemy.rs
@@ -26,14 +26,22 @@ pub fn spawn_enemy(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut
 		.insert(Enemy);
 }
 
+fn lerp(x1: f32, y1: f32, x2: f32, y2: f32, x: f32) -> f32 {
+	((x - x1) * y2 + (x2 - x) * y1) / (x2 - x1)
+}
+
 fn dister(heading: Vec2, target: Vec2, d0: f32) -> f32 {
 	let a = heading.dot(target.normalize());
 	let d = target.length();
-	if d < d0 {
-		-a * (1.0 - d / d0)
+	a * (if d < d0 {
+		lerp(0.0, -1.0, d0, 0.0, d)
+	} else if d < 3.0 * d0 {
+		lerp(d0, 0.0, 3.0 * d0, 1.0, d)
+	} else if d < 6.0 * d0 {
+		lerp(3.0 * d0, 1.0, 6.0 * d0, 0.0, d)
 	} else {
-		a
-	}
+		0.0
+	})
 }
 
 pub fn run_enemy(
@@ -48,7 +56,7 @@ pub fn run_enemy(
 			let mut result: f32 = 0.0;
 			let pos = enemy_tr.translation.xy();
 
-			result += dister(v, camera_pos.xy() - pos, 80.0);
+			result += dister(v, camera_pos.xy() - pos, 150.0);
 			for other in enemy_positions.iter() {
 				if (*other - pos).length() > 1e-5 {
 					result -= 0.5 * dister(v, *other - pos, 30.0);
@@ -67,7 +75,8 @@ pub fn run_enemy(
 			.map(|i| Vec2::from_angle((i as f32) * TAU / (GRADE_VECTORS as f32)))
 			.max_by(|v1, v2| grade(*v1).partial_cmp(&grade(*v2)).unwrap())
 			.unwrap();
+		let v_mod = (grade(target_v) / 0.7).clamp(0.0, 1.0);
 
-		enemy_tr.translation += (target_v * 2.0).extend(0.0);
+		enemy_tr.translation += (target_v * 2.0 * v_mod).extend(0.0);
 	}
 }
