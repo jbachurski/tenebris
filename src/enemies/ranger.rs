69591b01df8a9e4bed472f304c7216450f732e56
jbachurski <kbachurski@gmail.com>
2023-02-12 16:58:40 +0000

    Make the ranger shoot

--- src/enemies/ranger.rs
+++ src/enemies/ranger.rs
@@ -31,7 +31,9 @@ fn best_heading<F: FnMut(Vec2) -> f32>(n: usize, mut grade: F) -> Vec2 {
 }
 
 #[derive(Component)]
-pub struct EnemyRanger;
+pub struct EnemyRanger {
+	ticks: f32,
+}
 
 #[derive(Component, Deref, DerefMut)]
 pub struct RangerAnimationTimer(Timer);
@@ -53,7 +55,7 @@ pub fn spawn_ranger(
 			..default()
 		},
 		RangerAnimationTimer(Timer::from_seconds(0.05, TimerMode::Repeating)),
-		EnemyRanger,
+		EnemyRanger { ticks: 3.0 },
 		Bounded {
 			size: Vec2::splat(2. * 20.),
 		},
@@ -77,18 +79,21 @@ pub fn spawn_ranger(
 }
 
 pub fn run_ranger(
+	mut commands: Commands,
+	time: Res<Time>,
+	asset_server: Res<AssetServer>,
 	players: Query<&Transform, With<Player>>,
-	mut enemies: Query<(&Transform, &mut Velocity), (With<EnemyRanger>, Without<Player>)>,
+	mut enemies: Query<(&Transform, &mut Velocity, &mut EnemyRanger), Without<Player>>,
 	mut lines: ResMut<DebugLines>,
 ) {
-	let camera_pos = players.single().translation;
-	let enemy_positions: Vec<Vec2> = enemies.iter().map(|(t, _)| t.translation.xy()).collect();
-	for (enemy_tr, mut velocity) in enemies.iter_mut() {
+	let camera_pos = players.single().translation.xy();
+	let enemy_positions: Vec<Vec2> = enemies.iter().map(|(t, _, _)| t.translation.xy()).collect();
+	for (enemy_tr, mut velocity, mut ranger) in enemies.iter_mut() {
+		let pos = enemy_tr.translation.xy();
 		let mut grade = |v: Vec2| {
 			let mut result: f32 = 0.0;
-			let pos = enemy_tr.translation.xy();
 
-			result += dister(v, camera_pos.xy() - pos, 150.0);
+			result += dister(v, camera_pos - pos, 150.0);
 			for other in enemy_positions.iter() {
 				if (*other - pos).length() > 1e-5 {
 					result -= 0.5 * dister(v, *other - pos, 30.0);
@@ -107,6 +112,31 @@ pub fn run_ranger(
 		let v_mod = (grade(target_v) / 0.7).clamp(0.0, 1.0).sqrt();
 
 		velocity.linvel = target_v * 2.0 * v_mod * 60.;
+
+		let t = ranger.ticks - time.delta().as_secs_f32();
+
+		if t <= 0.0 {
+			commands
+				.spawn(SpriteBundle {
+					texture: asset_server.load("fire_bolt.png"),
+					transform: Transform::from_translation(pos.extend(2.0)),
+					..default()
+				})
+				.insert(PlayerDanger {
+					damage: 1,
+					hit_despawn: true,
+					til_despawn: 1.0,
+				})
+				.insert(Bounded { size: Vec2::splat(10.0) })
+				.insert(RigidBody::Dynamic)
+				.insert(Velocity {
+					linvel: (camera_pos - pos).normalize() * 300.0,
+					angvel: 0.0,
+				});
+			ranger.ticks = 2.0;
+		} else {
+			ranger.ticks = t;
+		}
 	}
 }
 
